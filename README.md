2205f29e2bfcca5c5c9ffb73e0d74043f8ed9454
Stu Bevan <stu@mees.st>
2019-03-14 20:54:11 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# brew-tap
+Work In Progress
+
+Install scripts onto Mees Machines!!
---
+++ system-scripts.rb
@@ -0,0 +1,12 @@
+class MeesSystemScripts < Formula
+  desc "System Scripts For Mees Machines"
+  homepage "https://github.com/stubevan/system-scripts"
+  url "https://github.com/stubevan/system-scripts-tap/system-scripts-v0.0.1.tar.gz"
+  sha256 "0fff94b3551875074b0ffdf1a09b16b6b936c5ae2e8fecf459e9c0b1d300dfd3"
+  version "0.0.1"
+
+  def install
+    # install scripts, create symlink to script in /usr/local/bin
+    bin.install "tarsnap.sh"
+  end
+end
