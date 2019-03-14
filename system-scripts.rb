cb10627780fc18fe945db0c65d2fe1fbdefc6543
Stu Bevan <stu@mees.st>
2019-03-14 21:42:45 +0100

    Correct URL

--- system-scripts.rb
+++ system-scripts.rb
@@ -1,7 +1,7 @@
 class SystemScripts < Formula
   desc "System Scripts For Mees Machines"
   homepage "https://github.com/stubevan/system-scripts"
-  url "https://github.com/stubevan/system-scripts-tap/system-scripts-v0.0.1.tar.gz"
+  url "https://github.com/stubevan/system-scripts-tap/raw/master/system-scripts-v0.0.1.tar.gz"
   sha256 "0fff94b3551875074b0ffdf1a09b16b6b936c5ae2e8fecf459e9c0b1d300dfd3"
   version "0.0.1"
 
